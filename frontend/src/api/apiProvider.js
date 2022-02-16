93f838819b3ceb1d2650d63ff656667674067504
BovizLaci <92304557+BovizLaci@users.noreply.github.com>
2022-02-16 14:56:06 +0100

    getArtworks and getArtwork in dataProvider

--- frontend/src/api/apiProvider.js
+++ frontend/src/api/apiProvider.js
@@ -1,32 +1,61 @@
 import axios from "axios";
 import { data } from "./sampleArtworks/sampleData";
 
-const apiURL = "https://collectionapi.metmuseum.org/public/collection/v1/search?q=monet"
+const apiSearchURL = "https://collectionapi.metmuseum.org/public/collection/v1/search?q=monet"
+const apiObjectURL = "https://collectionapi.metmuseum.org/public/collection/v1/objects/";
 
-export const getArtworks = async (itemsPerPage = 20, page = 1) => {
+export const getArtworkData = async (objectID, detailed = false) => {
+	const {data: artworkData} = await axios.get(apiObjectURL + objectID.toString());
+	let result;
+	if (detailed) {
+		result =
+		{
+			id: objectID,
+			title: artworkData.title,
+			period: artworkData.period,
+			artist:
+			{
+				name: artworkData.artistDisplayName,
+				born: artworkData.artistBeinDate,
+				died: artworkData.artistEndDate,
+				more: artworkData.artistWikidata_URL
+			},
+			dimensions: artworkData.dimensions,
+			creationYear: artworkData.objectEndDate,
+			city: artworkData.city,
+			country: artworkData.country,
+			rights: artworkData.rightsAndReproduction,
+			image: artworkData.primaryImage
+		}
+	} else {
+		result =
+		{
+			id: objectID,
+			title: artworkData.title,
+			artist: artworkData.artistDisplayName,
+			image: artworkData.primaryImageSmall
+		}
+	}
+	return result;
+}
+
+export const getArtworksData = async (itemsPerPage = 20, page = 1) => {
 	const resultData = [];
-	// const searchParams = {q: "monet"}
-	const response = await axios.get(apiURL);
-	// const response = await axios.get({
-		// 	url: "/search",
-		// 	baseURL: apiURL,
-		// 	params: searchParams
-		// });
-		const {data} = response;
-	const {objectIDs, total} = data;
+	const {data: {objectIDs, total}} = await axios.get(apiSearchURL);
 	const firstItem = (page - 1) * itemsPerPage;
 	const lastItem = firstItem + itemsPerPage;
-	
 	const objectIDsPage = objectIDs.slice(firstItem, lastItem);
 	const resultHasMorePage = (lastItem <= total);
 	for (const objectID of objectIDsPage) {
-		
+		const objectData = await getArtworkData(objectID, false);
+		resultData.push(objectData);
 	}
-	console.log(objectIDsPage);
-	return {
+	const result = {
 		artworks: resultData,
 		hasMorePage: resultHasMorePage
 	}
+	console.log(result);
+	return result;
 
 	// console.log(data);
 	// return await data;	
--- frontend/src/component/Sample.jsx
+++ frontend/src/component/Sample.jsx
@@ -1,15 +1,16 @@
 import { useEffect, useState } from "react";
-import { getArtworks } from "../api/apiProvider";
+import { getArtworksData } from "../api/apiProvider";
 
 function Sample() {
 
-	const __dataPath = "api/sampleArtworks"
 
 	const [artworks, setArtworks] = useState([]);
+	const [hasMorePage, setHasMorePage] = useState();
 
 	const loadArtworks = async () => {
-		const data = await getArtworks(10, 1);
-		setArtworks(data);
+		const data = await getArtworksData(10, 18);
+		setArtworks(data.artworks);
+		setHasMorePage(data.hasMorePage);
 	}
 
 	useEffect(() => {
@@ -20,9 +21,9 @@ function Sample() {
 		<div>
 			{artworks.map((artwork, index) => {
 				return (
-					<div>
-						<p key={index}>{artwork.title}</p>
-						<img src={__dataPath + artwork.images.small} alt={artwork.title} />
+					<div key={`div${index}`}>
+						<p key={`p${index}`}>{artwork.title}</p>
+						<img  key={`img${index}`} src={artwork.image} alt={artwork.title} />
 					</div>
 				)
 			})};
