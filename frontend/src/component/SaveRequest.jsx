79c8e25d111ba32ffb5c4726dcd7a352730a699c
GegenyPeter <gegeny.p@gmail.com>
2022-02-18 00:03:46 +0100

    save request done, collection comp almost done

---
+++ frontend/src/component/SaveRequest.jsx
@@ -0,0 +1,22 @@
+import axios from "axios";
+
+export const SaveArtwork = async (data) => {
+  console.log(data);
+  const newArt = {
+    id: data.id,
+    title: data.title,
+    artist: data.artistName,
+    image: data.image,
+  };
+  try {
+    await axios.post("http://localhost:4000/api/save", newArt);
+    alert("Artwork saved!");
+  } catch (err) {
+    if (!err.response) {
+      alert("Something went wrong");
+    }
+    if (err.response.status === 409) {
+      alert("Artwork already saved!");
+    }
+  }
+};
