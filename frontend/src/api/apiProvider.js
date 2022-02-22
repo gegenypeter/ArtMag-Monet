41cd1e5a577a7c1acfa28f9f55288a61df32235d
BovizLaci <92304557+BovizLaci@users.noreply.github.com>
2022-02-22 05:35:53 +0100

    fixed search

--- frontend/src/api/apiProvider.js
+++ frontend/src/api/apiProvider.js
@@ -1,10 +1,15 @@
+import { useState, useEffect } from "react";
 import axios from "axios";
 
-const apiSearchURL = "https://collectionapi.metmuseum.org/public/collection/v1/search?hasImages=true&artisOrCulture"
-const apiObjectURL = "https://collectionapi.metmuseum.org/public/collection/v1/objects/";
+const mainUrls = {
+	search: "https://collectionapi.metmuseum.org/public/collection/v1/search?hasImages=true&artisOrCulture",
+	object: "https://collectionapi.metmuseum.org/public/collection/v1/objects/"
+  };
+
+const itemsPerPage = 8;
 
 export const getArtworkData = async (objectID, detailed = true) => {
-	const {data} = await axios.get(apiObjectURL.concat(objectID.toString()));
+	const {data} = await axios.get(mainUrls.object.concat(objectID.toString()));
 	
 	let result;
 	if (detailed) {
@@ -44,8 +49,8 @@ export const getArtworkData = async (objectID, detailed = true) => {
 
 export const getResultSet = async (query) => {
 	if (!query) query = "monet";
-	const {data: {objectIDs}} = await axios.get(apiSearchURL.concat("&q=", query));
-	return objectIDs;
+	const {data: {objectIDs}} = await axios.get(mainUrls.search.concat("&q=", query));
+	return	 objectIDs;
 }
 
 export const getArtworkPageData = async (objctIDsPage) => {
@@ -56,3 +61,77 @@ export const getArtworkPageData = async (objctIDsPage) => {
 	}
 	return pageData;
 }
+
+// ======================================
+
+export const useArtworks = (initSearchExpr) => {
+	
+	const [hasMorePage, setHasMorePage] = useState(false);
+	const [searchExpr, setSearchExpr] = useState(initSearchExpr);
+	const [artworks, setArtworks] = useState([]);
+	const [objectIDs, setObjectIDs] = useState([]);
+	const [lastPage, setLastPage] = useState(0);
+	const [resultNo, setResultNo] = useState();
+	
+	const fetchObjectIDs = async () => {
+		const {data: {objectIDs}} = await axios.get(mainUrls.search.concat("&q=", (!searchExpr) ? "Monet" : searchExpr));
+		return objectIDs;
+	}
+	
+	const setResult = async () => {
+		setLastPage(0);
+		setArtworks([]);
+		setHasMorePage(false);
+		const oIds = await fetchObjectIDs();
+		setResultNo(0);
+		setObjectIDs(oIds);
+		setResultNo(oIds.length);
+		setLastPage(1);
+	}
+
+	useEffect(() => {
+		setResult();
+	}, [searchExpr]);
+
+
+	const fetchNextPageOfArtworks = async () => {
+		let nextPageOfArtworks = [];
+		let _hasMorePage = false;
+		if ((lastPage > 0) && (objectIDs.length > 0)) {
+			const total = objectIDs.length;
+			const firstItem = (lastPage - 1) * itemsPerPage;
+			const lastItem = firstItem + itemsPerPage;
+			const objectIDsPage = objectIDs.slice(firstItem, lastItem);
+			_hasMorePage = (lastItem <= total);
+			for (const objectID of objectIDsPage) {
+				const nextArtwork = await getArtworkData(objectID, false);
+				nextPageOfArtworks.push(nextArtwork);
+			}
+		}
+		return {
+			nextPage: nextPageOfArtworks,
+			hasMore: _hasMorePage
+		}
+	}		
+
+	const setNextPage = async () => {
+		const result = await fetchNextPageOfArtworks();
+		setArtworks([].concat(...artworks, result.nextPage));
+		setHasMorePage(result.hasMore);
+	}
+	
+	useEffect(() => {
+		setNextPage();
+	}, [lastPage]);
+	
+	return [
+		((artworks.length === 0) && (resultNo > 0)) ? "Loading..." : artworks,
+		searchExpr,
+		setSearchExpr,
+		lastPage,
+		setLastPage,
+		hasMorePage,
+		resultNo
+	];
+};
+		
