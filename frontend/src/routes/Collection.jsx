dbe9b034a5726476482ff281af80925ad31a3d6d
GegenyPeter <gegeny.p@gmail.com>
2022-02-23 20:07:59 +0100

    collection page working

--- frontend/src/routes/Collection.jsx
+++ frontend/src/routes/Collection.jsx
@@ -2,39 +2,31 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import "../styles/Collection.css";
 
-const Collection = () => {
-  const [saved, setSaved] = useState([]);
-  const sessionId = localStorage.getItem('sessionId');
-  console.log(sessionId);
-
+const Collection = (props) => {
+  const {authEmail, userArtworks, setUserArtworks} = props
   async function load() {
-    const response = await axios("http://localhost:4000/api/collection",{}, {headers: {
-      authorization: sessionId,
-    }})
-    setSaved(response.data);
-  };
-
-  useEffect(() => {
-    try {
-      load();
+    console.log(authEmail);
+    const response = await axios(
+      `http://localhost:4000/api/collection`,{params: {email: authEmail }});
+      setUserArtworks(await response.data);
     }
-    catch(err){}
-    if (!sessionId) return
+    
+    useEffect(() => {
+      load();
+      console.log(userArtworks);
   }, []);
-
-
 
   return (
     <>
       <h1>My collection</h1>
       <div className="collection">
-        {saved.map((art) => (
-          <div className="thumbNailDiv" key={art.artworks.id}>
-            <img className="smallImg" alt="" src={art.artworks.image} />
-            <div className="contentDiv">
-              <p className="artWorkTitle">{art.artworks.title}</p>
-              <p className="artistName">Artist: {art.artworks.artist}</p>
-            </div>
+        {userArtworks.length && userArtworks.map((art) => (
+          <div className="thumbNailDiv" key={art.id}>
+             <img className="smallImg" alt="" src={art.image} />
+             <div className="contentDiv">
+              <p className="artWorkTitle">{art.title}</p>
+              <p className="artistName">Artist: {art.artist}</p>
+             </div>
           </div>
         ))}
       </div>
